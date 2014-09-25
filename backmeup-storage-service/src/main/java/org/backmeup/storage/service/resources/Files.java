1c87193b29f2ee4fd599cda13131d5309a447164
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-09-25 14:42:59 +0200

    Added stub methods to up/download a file

--- backmeup-storage-service/src/main/java/org/backmeup/storage/service/resources/Files.java
+++ backmeup-storage-service/src/main/java/org/backmeup/storage/service/resources/Files.java
@@ -1,6 +1,14 @@
 package org.backmeup.storage.service.resources;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,4 +16,18 @@ import org.slf4j.LoggerFactory;
 @Path("/files")
 public class Files {
 	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+	
+	@GET
+    @Path("/{path}")
+	@Produces(MediaType.APPLICATION_OCTET_STREAM)
+	public Response getFile(@PathParam("path") String filePath) {
+		return Response.ok().build();
+	}
+	
+	@PUT
+    @Path("/{path}")
+	@Produces(MediaType.APPLICATION_OCTET_STREAM)
+	public Response putFile(@Context HttpServletRequest request, @PathParam("path") String filePath) {
+		return Response.ok().build();
+	}
 }
