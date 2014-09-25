f6401b7c01f0c1f566dc1eba78422303b1dd6537
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-09-25 14:55:06 +0200

    Renamed application class and added logback config and web.xml

--- backmeup-storage-service/src/main/java/org/backmeup/storage/service/BackmeupStorageApplicaiton.java
+++ backmeup-storage-service/src/main/java/org/backmeup/storage/service/BackmeupStorageApplicaiton.java
@@ -8,11 +8,11 @@ import javax.ws.rs.core.Application;
 import org.backmeup.storage.service.filters.TimingResourceFilter;
 import org.backmeup.storage.service.resources.Files;
 
-public class StorageApplicaiton extends Application {
+public class BackmeupStorageApplicaiton extends Application {
 	private final Set<Class<?>> set = new HashSet<>();
     private final Set<Object> singletons = new HashSet<>();
 
-    public StorageApplicaiton() {
+    public BackmeupStorageApplicaiton() {
         singletons.add(new Files());
 
         set.add(TimingResourceFilter.class);
