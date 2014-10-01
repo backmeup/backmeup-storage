5c4e79d24007239350b2fe948263234e11240aca
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-10-01 16:21:33 +0200

    Return metadata of the uploaded file (put request)

---
+++ backmeup-storage-service/src/main/java/org/backmeup/storage/model/utils/StringUtils.java
@@ -0,0 +1,39 @@
+package org.backmeup.storage.model.utils;
+
+import java.io.UnsupportedEncodingException;
+
+// taken from http://www.rgagnon.com/javadetails/java-0596.html
+public class StringUtils {
+
+	static final byte[] HEX_CHAR_TABLE = { (byte) '0', (byte) '1', (byte) '2',
+			(byte) '3', (byte) '4', (byte) '5', (byte) '6', (byte) '7',
+			(byte) '8', (byte) '9', (byte) 'a', (byte) 'b', (byte) 'c',
+			(byte) 'd', (byte) 'e', (byte) 'f' };
+
+	private StringUtils() {
+	}
+
+	public static String getHexString(byte[] raw)
+			throws UnsupportedEncodingException {
+		byte[] hex = new byte[2 * raw.length];
+		int index = 0;
+
+		for (byte b : raw) {
+			int v = b & 0xFF;
+			hex[index++] = HEX_CHAR_TABLE[v >>> 4];
+			hex[index++] = HEX_CHAR_TABLE[v & 0xF];
+		}
+		return new String(hex, "ASCII");
+	}
+
+	public static void main(String args[]) throws Exception {
+		byte[] byteArray = { (byte) 255, (byte) 254, (byte) 253, (byte) 252,
+				(byte) 251, (byte) 250 };
+
+		System.out.println(StringUtils.getHexString(byteArray));
+
+		/*
+		 * output : fffefdfcfbfa
+		 */
+	}
+}
--- backmeup-storage-service/src/main/java/org/backmeup/storage/service/resources/Files.java
+++ backmeup-storage-service/src/main/java/org/backmeup/storage/service/resources/Files.java
@@ -5,6 +5,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.DefaultValue;
@@ -21,11 +25,14 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.backmeup.storage.model.dto.Metadata;
+import org.backmeup.storage.model.utils.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Path("/files")
 public class Files {
+	private static final String DIGEST_ALGORITHM = "MD5";
 	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 	private static final String BASE_PATH = "/data/backmeup-storage/test/";
 	
@@ -58,18 +65,38 @@ public class Files {
 		if (file != null  && file.exists() && !overwrite) {
 			throw new WebApplicationException(Status.CONFLICT);
 		}
+		
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance(DIGEST_ALGORITHM);
+		} catch (NoSuchAlgorithmException e) {
+			LOGGER.error("", e);
+		}
+		long totalLength = 0;
+		
 		try (OutputStream out = new FileOutputStream(file);) {
 			InputStream content = request.getInputStream();
 			byte[] buffer = new byte[1024];
 			int len;
 			while ((len = content.read(buffer)) != -1) {
 				out.write(buffer, 0, len);
+				md.update(buffer, 0, len);
+				totalLength += len;
 			}
 		} catch (IOException e) {
 			LOGGER.error("", e);
 			throw new WebApplicationException(Status.INTERNAL_SERVER_ERROR);
-		} 
+		}
+		
+		String hash;
+		try {
+			hash = StringUtils.getHexString(md.digest());
+		} catch (UnsupportedEncodingException e) {
+			LOGGER.error("", e);
+			hash = "-1";
+		}
+		Metadata fileMetadata = new Metadata(totalLength, hash, new Date(), filePath);
 
-		return Response.ok().build();
+		return Response.ok(fileMetadata).build();
 	}
 }
