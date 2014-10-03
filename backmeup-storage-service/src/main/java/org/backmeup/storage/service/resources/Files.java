c28c8941b329c124ad1c136fe5e4816b1dc422ae
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2014-10-03 16:14:15 +0200

    Removed BL code from rest resource and use dedicated BL.

--- backmeup-storage-service/src/main/java/org/backmeup/storage/service/resources/Files.java
+++ backmeup-storage-service/src/main/java/org/backmeup/storage/service/resources/Files.java
@@ -1,15 +1,9 @@
 package org.backmeup.storage.service.resources;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Date;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
@@ -25,22 +19,25 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.backmeup.storage.logic.StorageLogic;
 import org.backmeup.storage.model.dto.Metadata;
-import org.backmeup.storage.model.utils.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Path("/files")
 public class Files {
-	private static final String DIGEST_ALGORITHM = "MD5";
-	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
-	private static final String BASE_PATH = "/data/backmeup-storage/test/";
+//	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 	
+	@Inject
+	private StorageLogic storageLogic;
+	
+	public StorageLogic getStorageLogic() {
+		return storageLogic;
+	}
+
 	@GET
     @Path("/{path}")
 	@Produces(MediaType.APPLICATION_OCTET_STREAM)
 	public Response getFile(@PathParam("path") String filePath) {
-		File file = new File(BASE_PATH + filePath);
+		File file = getStorageLogic().getFile(filePath);
 		if (file == null || !file.exists()) {
 			throw new WebApplicationException(Status.NOT_FOUND);
 		}
@@ -56,47 +53,12 @@ public class Files {
 	public Response putFile(@Context HttpServletRequest request, 
 							@PathParam("path") String filePath, 
 							@QueryParam("overwrite") @DefaultValue("false") boolean overwrite,
-							@HeaderParam("Content-Length") long contentLength) {
+							@HeaderParam("Content-Length") long contentLength) throws IOException {
 		if (contentLength <= 0) {
 			throw new WebApplicationException(Status.LENGTH_REQUIRED);
 		}
 		
-		File file = new File(BASE_PATH + filePath);
-		if (file != null  && file.exists() && !overwrite) {
-			throw new WebApplicationException(Status.CONFLICT);
-		}
-		
-		MessageDigest md = null;
-		try {
-			md = MessageDigest.getInstance(DIGEST_ALGORITHM);
-		} catch (NoSuchAlgorithmException e) {
-			LOGGER.error("", e);
-		}
-		long totalLength = 0;
-		
-		try (OutputStream out = new FileOutputStream(file);) {
-			InputStream content = request.getInputStream();
-			byte[] buffer = new byte[1024];
-			int len;
-			while ((len = content.read(buffer)) != -1) {
-				out.write(buffer, 0, len);
-				md.update(buffer, 0, len);
-				totalLength += len;
-			}
-		} catch (IOException e) {
-			LOGGER.error("", e);
-			throw new WebApplicationException(Status.INTERNAL_SERVER_ERROR);
-		}
-		
-		String hash;
-		try {
-			hash = StringUtils.getHexString(md.digest());
-		} catch (UnsupportedEncodingException e) {
-			LOGGER.error("", e);
-			hash = "-1";
-		}
-		Metadata fileMetadata = new Metadata(totalLength, hash, new Date(), filePath);
-
+		Metadata fileMetadata = getStorageLogic().saveFile(filePath, overwrite, contentLength, request.getInputStream());
 		return Response.ok(fileMetadata).build();
 	}
 }
