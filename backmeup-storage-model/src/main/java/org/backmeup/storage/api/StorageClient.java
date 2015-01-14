0e98000e6e29c9b7d2f132e1e9b495faafdd70fa
Sebastian Pimminger <sebastian.pimminger@gmail.com>
2015-01-14 13:58:47 +0100

    Moved client interface to model project

--- backmeup-storage-model/src/main/java/org/backmeup/storage/api/StorageClient.java
+++ backmeup-storage-model/src/main/java/org/backmeup/storage/api/StorageClient.java
@@ -1,4 +1,4 @@
-package org.backmeup.storage.client;
+package org.backmeup.storage.api;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -7,6 +7,8 @@ import java.io.OutputStream;
 import org.backmeup.storage.model.Metadata;
 
 public interface StorageClient {
+    String authenticate(String username, String password) throws IOException;
+    
     Metadata saveFile(String accessToken, String path, boolean overwrite, long numBytes, InputStream data) throws IOException;
 
     void getFile(String accessToken, String path, OutputStream data) throws IOException;
